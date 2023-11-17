04ca970218d3901c14376292f66b20b212997761
kimminji000 <minji6764@naver.com>
2023-11-17 21:22:14 +0900

    기초 레이아웃

---
+++ app/src/main/java/com/example/cafeseatfinder/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.cafeseatfinder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    MapFragment mapFragment;
+    NotiFragment notiFragment;
+    MypageFragment mypageFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        mapFragment = new MapFragment();
+        notiFragment = new NotiFragment();
+        mypageFragment = new MypageFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_nav);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.map:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mapFragment).commit();
+                        return true;
+                    case R.id.noti:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notiFragment).commit();
+                        return true;
+                    case R.id.mypage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mypageFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
