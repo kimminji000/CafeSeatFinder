65de9e75789aeb2cff64f6fd90ea7e23157e43a0
kimminji000 <minji6764@naver.com>
2023-11-18 22:52:23 +0900

    네이버지도 연결

--- app/src/main/java/com/example/cafeseatfinder/MainActivity.java
+++ app/src/main/java/com/example/cafeseatfinder/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.cafeseatfinder;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         homeFragment = new HomeFragment();
         mapFragment = new MapFragment();
